42edab1aca0e9d90e083ba93f6e67139bab20117
AvatarRodrigo <rodrigo@avatarsports.com.br>
2021-08-10 09:55:21 -0300

    inicio layout login

--- app/src/main/java/com/academia/crosstrainer/activity/CadastroActivity.java
+++ app/src/main/java/com/academia/crosstrainer/activity/CadastroActivity.java
@@ -11,6 +11,6 @@ public class CadastroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intro_login);
+        setContentView(R.layout.activity_cadastro);
     }
 }
