569e4b80ee6d449935a1b6b516e709f456c365f9
AvatarRodrigo <rodrigo@avatarsports.com.br>
2021-08-10 09:45:30 -0300

    inicio layout login

--- app/src/main/java/com/academia/crosstrainer/IntroActivity.java
+++ app/src/main/java/com/academia/crosstrainer/IntroActivity.java
@@ -1,9 +1,12 @@
 package com.academia.crosstrainer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
+import com.academia.crosstrainer.activity.CadastroActivity;
+import com.academia.crosstrainer.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class IntroActivity extends com.heinrichreimersoftware.materialintro.app.IntroActivity {
@@ -14,7 +17,7 @@ public class IntroActivity extends com.heinrichreimersoftware.materialintro.app.
         //setContentView(R.layout.activity_intro);
 
         setButtonBackVisible(false);
-        setButtonNextVisible(false);
+        setButtonNextVisible(true);
         setButtonCtaVisible(false);
 
         //autoplay(3500,1);
@@ -44,9 +47,28 @@ public class IntroActivity extends com.heinrichreimersoftware.materialintro.app.
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.background_dark)
                 .fragment(R.layout.intro4)
+                .canGoBackward(false)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.background_dark)
+                .fragment(R.layout.intro5)
+                .canGoBackward(false)
                 .canGoForward(false)
                 .build()
         );
 
     }
+    public void btnEnter(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btnRegister(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+    public void btnWant(View view){
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://acadtrainer.com.br")));
+    }
+
 }
---
+++ app/src/main/java/com/academia/crosstrainer/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.academia.crosstrainer.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.academia.crosstrainer.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_login);
+    }
+}
